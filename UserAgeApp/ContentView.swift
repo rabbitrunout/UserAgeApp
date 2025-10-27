7cec9bd440c3c6e0e73b4b4e389c6b45526f8d09
Irina Saf <irina.safronova0801@gmail.com>
2025-10-27 08:12:31 -0400

    Initial Commit

---
+++ UserAgeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserAgeApp
+//
+//  Created by Irina Saf on 2025-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserAgeApp/UserAgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAgeAppApp.swift
+//  UserAgeApp
+//
+//  Created by Irina Saf on 2025-10-27.
+//
+
+import SwiftUI
+
+@main
+struct UserAgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
